2302f7e33aa9ed5f1d36e8130537b9fd6400e294
Charlie <huh4142@naver.com>
2020-10-18 19:45:00 +1100

    lesson-19 Basic Routing

--- app.js
+++ app.js
@@ -7,14 +7,20 @@ const fs = require('fs');
 // whenever we send a reqeust to the server, the function will fire
 const server = http.createServer((req, res) => {
   console.log('requrest was made: ' + req.url)
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  
-  const myObj = {
-    name: 'ryu',
-    job: 'ninja',
-    age: 29
-  };
-  res.end(JSON.stringify(myObj));
+  if(req.url === '/home' || req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  } else if(req.url === '/contact') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/contact.html').pipe(res);
+  } else if(req.url === '/api/ninjas') {
+    const ninjas = [{name: 'mario', age: 29}, {name: 'yoshi', age:24}];
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(ninjas));
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/404.html').pipe(res);
+  }
 });
 
 server.listen(3000, '127.0.0.1');
