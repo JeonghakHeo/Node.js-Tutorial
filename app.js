dc1bfd6aa93c228dabe93de95fd43650c0ef0519
Charlie <huh4142@naver.com>
2020-10-18 13:14:02 +1100

    lesson-7 Module Patterns

---
+++ app.js
@@ -0,0 +1,8 @@
+const stuff = require('./stuff');
+
+const fruits = ['apple', 'orange', 'banana', 'pineapple', 'melon'];
+
+console.log(stuff.counter(fruits));
+console.log(stuff.adder(1,3));
+console.log(stuff.pi);
+console.log(stuff.multiply(4,2));
