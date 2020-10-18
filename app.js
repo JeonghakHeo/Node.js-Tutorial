fd8a18a87b94c2c83c56af35a5b72675895b4891
Charlie <huh4142@naver.com>
2020-10-18 18:59:52 +1100

    lesson-16 Pipes

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 const http = require('http');
-
 const fs = require('fs');
 
-const myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf-8');
-const myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+// we don't have to manually listen for data event, instead pipes will automatically do this process
 
-myReadStream.on('data', (chunk) => {
-  console.log('new chunk received');
-  myWriteStream.write(chunk);
-});
-/*
+
 // whenever we send a reqeust to the server, the function will fire
 const server = http.createServer((req, res) => {
   console.log('requrest was made: ' + req.url)
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hey ninjas');
+  const myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf-8');
+  myReadStream.pipe(res);
+   
 });
 
 server.listen(3000, '127.0.0.1');
 console.log('lisetning to port 3000');
 
-*/
