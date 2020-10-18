bf84c2aea7040549849c9a748f3de04032feb892
Charlie <huh4142@naver.com>
2020-10-18 18:41:47 +1100

    lesson-14 Readable Streams

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const http = require('http');
 
-// whenever we senda reqeust to the server, the function will fire
+const fs = require('fs');
+
+const myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf-8');
+
+myReadStream.on('data', (chunk) => {
+  console.log('new chunk received');
+  console.log(chunk)
+})
+/*
+// whenever we send a reqeust to the server, the function will fire
 const server = http.createServer((req, res) => {
   console.log('requrest was made: ' + req.url)
   res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -9,3 +18,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, '127.0.0.1');
 console.log('lisetning to port 3000');
+
+*/
