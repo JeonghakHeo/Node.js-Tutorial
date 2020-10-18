62ce68295110d412ad59caaea73f6a2ad01d821f
Charlie <huh4142@naver.com>
2020-10-18 18:21:17 +1100

    lesson-12 Creating a Server

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-const fs = require('fs');
+const http = require('http');
 
-// delete file
-fs.unlink('writeMe.txt', () => {});
+// whenever we senda reqeust to the server, the function will fire
+const server = http.createServer((req, res) => {
+  console.log('requrest was made: ' + req.url)
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hey ninjas');
+});
 
-// fs.mkdirSync('sundry', () => {})
-
-fs.mkdir('sundry', () => {
-  fs.readFile('readMe.txt', 'utf-8', (err, data) => {
-    fs.writeFile('./sundry/writeMe.txt', data, () => {});
-  });
-})
-
-fs.unlink('./sundry/writeMe.txt', () => {});
-fs.rmdir('sundry', () => {});
+server.listen(3000, '127.0.0.1');
+console.log('lisetning to port 3000');
