e7115ad41475dce20542e087050e6be6e9a881e2
Charlie <huh4142@naver.com>
2020-10-18 18:47:44 +1100

    lesson-15 Writable Streams

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const http = require('http');
 const fs = require('fs');
 
 const myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf-8');
+const myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
 myReadStream.on('data', (chunk) => {
   console.log('new chunk received');
-  console.log(chunk)
-})
+  myWriteStream.write(chunk);
+});
 /*
 // whenever we send a reqeust to the server, the function will fire
 const server = http.createServer((req, res) => {
