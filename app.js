cf2261b7395434fdc892401d2bb824c226eb8f79
Charlie <huh4142@naver.com>
2020-10-20 08:58:54 +1100

    lesson-23 Introduction to Express

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-// we don't have to manually listen for data event, instead pipes will automatically do this process
+const app = express();
 
-
-// whenever we send a reqeust to the server, the function will fire
-const server = http.createServer((req, res) => {
-  console.log('requrest was made: ' + req.url)
-  if(req.url === '/home' || req.url === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
-  } else if(req.url === '/contact') {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/contact.html').pipe(res);
-  } else if(req.url === '/api/ninjas') {
-    const ninjas = [{name: 'mario', age: 29}, {name: 'yoshi', age:24}];
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(ninjas));
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/404.html').pipe(res);
-  }
+app.get('/', (req, res)=> {
+  res.send('this is the homepage');
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('lisetning to port 3000');
+app.get('/contact', (req, res)=> {
+  res.send('this is the contact');
+});
 
+app.listen(3000);
