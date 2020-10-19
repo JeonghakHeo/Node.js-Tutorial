ab4d0bd915bf86cd5e4953c534b75127958e7139
Charlie <huh4142@naver.com>
2020-10-20 09:08:25 +1100

    lesson-24 Express Route Params

--- app.js
+++ app.js
@@ -10,4 +10,12 @@ app.get('/contact', (req, res)=> {
   res.send('this is the contact');
 });
 
+app.get('/profile/:id', (req, res) => {
+  res.send('You requested to see a profile with the id of ' + req.params.id);
+});
+
+app.get('/user/:name', (req, res) => {
+  res.send('You requested to see a profile with the name of ' + req.params.name);
+});
+
 app.listen(3000);
